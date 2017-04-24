482c7a720287a8e6a7591d440170f8c4b2a911b4
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2017-04-24 08:22:47 +0200

    Refactor deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-images="controller invoker"
-for i in $images
-do
-    docker pull "openwhisk/$i"
-    docker tag "openwhisk/$i" $i
-done
+# checkout OpenWhisk latest
+git clone --depth 1 https://github.com/openwhisk/openwhisk.git
+
+# install ansible
+pip install --user ansible==2.1.2.0
+
+cd openwhisk/ansible
+ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=openwhisk"
+
+$ANSIBLE_CMD setup.yml
+$ANSIBLE_CMD prereq.yml
+$ANSIBLE_CMD couchdb.yml
+$ANSIBLE_CMD initdb.yml
+$ANSIBLE_CMD apigateway.yml
+$ANSIBLE_CMD wipe.yml
+$ANSIBLE_CMD openwhisk.yml
