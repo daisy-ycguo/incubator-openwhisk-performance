c092f1ac15ee0796645b5fe4c82ad648e28ef74c
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2017-04-24 09:49:20 +0200

    Actually use a performance test

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+action="noop"
+
 # create a noop action
-curl -u "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/curlTest" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
+echo "Creating noop action"
+curl -u "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
 
 # run the noop action
-curl -u "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/curlTest?blocking=true" -XPOST
+echo "Running noop action once to assert an intact system"
+curl -u "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/$action?blocking=true" -XPOST
+
+# run performance harness
+ab -n 1000 -m POST -k -A "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/$action?blocking=true"
