9a0cf499a1f9f46955b09cc2ccce7e1ac2b67a3e
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2017-04-24 13:47:44 +0200

    Refactor test setup

--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
+set -e
 
 action="noop"
+credentials="$(cat openwhisk/ansible/files/auth.guest)"
+host="172.17.0.1:10001"
 
 # create a noop action
 echo "Creating noop action"
-curl -u "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
+curl -u "$credentials" "$host/api/v1/namespaces/_/actions/$action" -XPUT -d '{"namespace":"_","name":"test","exec":{"kind":"nodejs:default","code":"function main(){return {};}"}}' -H "Content-Type: application/json"
 
 # run the noop action
 echo "Running noop action once to assert an intact system"
-curl -u "$(cat openwhisk/ansible/files/auth.guest)" "172.17.0.1:10001/api/v1/namespaces/_/actions/$action?blocking=true" -XPOST
+curl -u "$credentials" "$host/api/v1/namespaces/_/actions/$action?blocking=true" -XPOST
 
 # run performance harness
-docker run --rm markusthoemmes/loadtest loadtest -n 10000 -H "Authorization: basic MjNiYzQ2YjEtNzFmNi00ZWQ1LThjNTQtODE2YWE0ZjhjNTAyOjEyM3pPM3haQ0xyTU42djJCS0sxZFhZRnBYbFBrY2NPRnFtMTJDZEFzTWdSVTRWck5aOWx5R1ZDR3VNREdJd1A=" "http://172.17.0.1:10001/api/v1/namespaces/_/actions/$action?blocking=true" -m POST
+docker run --rm markusthoemmes/loadtest loadtest -n 10000 -H "Authorization: basic $(echo $credentials | base64 -w 0)" "http://$host/api/v1/namespaces/_/actions/$action?blocking=true" -m POST
